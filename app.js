25419d6f579a1659470b1f5846e779a9f1c2826b
Marce-lr <marceitha123@gmail.com>
2025-08-27 23:24:11 -0600

    Configuración del Endpoint 1

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("¡Git + Node.js básico! servidor con Node.js y Express.");
 });
 
+app.get("/endpoint1", (req, res) => {
+  res.send("¡Este es el Endpoint 1!");
+});
+
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
